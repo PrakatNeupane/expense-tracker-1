7a31bea2f33077e1d66bcce4d33b8ce94781570b
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-03-06 08:34:35 +1100

    props works, previously didnt work because toISOString() was missing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'; // You can simply import this in all the files with JSX code just so that you remember what happens behind the hood 
 import { ExpenseItem } from './components/Expenses/ExpenseItem';
-import './components/Expenses/ExpenseItem.css'
 
 const App = () => {
 
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./ExpenseItem.css";
 
-export const ExpenseItem = ({ title, date, amount }) => {
+export const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>{title}</div>
+      <div>{props.title}</div>
       <div className="expense-item__description">
-        <h2>{date}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <h2>{props.date.toISOString()}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
