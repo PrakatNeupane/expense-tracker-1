b87d9c68f9b5931e93679b905d6d367a5e3f6a51
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-03-07 15:49:43 +1100

    created a new component Expenses.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'; // You can simply import this in all the files with JSX code just so that you remember what happens behind the hood 
-import { ExpenseItem } from './components/Expenses/ExpenseItem';
+import { Expenses } from './components/Expenses/Expenses';
 
 const App = () => {
 
@@ -37,12 +37,7 @@ const App = () => {
   ]
 
   return (
-    <div>
-      <ExpenseItem title={expenses[0].title} date={expenses[0].date} amount={expenses[0].amount} />
-      <ExpenseItem title={expenses[1].title} date={expenses[1].date} amount={expenses[1].amount} />
-      <ExpenseItem title={expenses[2].title} date={expenses[2].date} amount={expenses[2].amount} />
-      <ExpenseItem title={expenses[3].title} date={expenses[3].date} amount={expenses[3].amount} />
-    </div>
+    <Expenses expenses={expenses} />
   )
 }
 export default App;
